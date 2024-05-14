268114afc5440c8abd8b45d3fe68e24b26bfc7a2
“juancookz” <jcavila383@gmail.com>
2024-05-14 11:54:59 -0300

    Ejercicio 1 realizado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+﻿Console.WriteLine("Ingrese un numero positivo a invertir");
+int num = int.Parse(Console.ReadLine());
+if (num > 0)
+{
+    num = num - (num * 2);
+    Console.WriteLine("El numero invertido es: " + num);
+}
+else
+{
+    Console.WriteLine("El numero ingresado no es mayor a 0");
+}
